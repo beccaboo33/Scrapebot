622ee5fd3e03ccf15c0d79407698cdc0f4130d91
Rebecca <35747856+beccaboo33@users.noreply.github.com>
2019-05-22 22:43:42 -0400

    Add files via upload

---
+++ quotesbot/spiders/quotescript.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 19:10:45 2019
+
+@author: BeccaYiu
+"""
+
+import scrapy
+import json
+
+
+class ToScrapeSpiderXPath(scrapy.Spider):
+    name = 'toscrape-xpath'
+    start_urls = [
+        '   /',
+    ]
+
+    def parse(self, response):
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
+                'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
+                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
+            }
+
+        next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
+
+
+class JsonWriterPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('items.jl', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+            
+
